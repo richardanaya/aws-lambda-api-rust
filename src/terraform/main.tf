1e0ac829042fdbde173e3f6355c5f9bba9c2d6b3
Richard Anaya <richard.anaya@gmail.com>
2018-08-26 21:03:24 +0000

    smart zipping

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "zip" {
 	triggers { uuid = "${uuid()}" } 
 	provisioner "local-exec" {
-		# Create zip if binary is newer, otherwise do nothing
-		command = "test `find ../lambdas/ -name rust-api -newer ../lambdas/rust-api.zip` && zip -9 ../lambdas/rust-api.zip ../lambdas/rust-api || true"
+		# Create zip if zip doesn't exist or binary is newer than zip
+		command = "test -e ../lambdas/rust-api.zip && test -z `find ../lambdas/ -name rust-api -newer ../lambdas/rust-api.zip` || zip -9 -j ../lambdas/rust-api.zip ../lambdas/rust-api"
 	}
 }
