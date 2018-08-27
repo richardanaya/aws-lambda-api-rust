3d0d9e48dd57d9853bc2b18578423919a97183fb
richard <richard>
2018-08-27 15:55:20 +0000

    Adding proxy api gateway

--- src/lambdas/rust-api/src/main.rs
+++ src/lambdas/rust-api/src/main.rs
@@ -1,6 +1,13 @@
 extern crate aws_lambda as lambda;
+#[macro_use]
+extern crate serde_json;
 
 fn main() {
     // start the runtime, and return a greeting every time we are invoked
-    lambda::start(|()| Ok("Hello ƛ!"))
+    lambda::start(|()| Ok(
+        json!({
+          "statusCode":200,
+          "body":"Hello World!"
+      })
+    ))
 }
--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -25,4 +27,56 @@ resource "aws_lambda_function" "rust-api_lambda" {
   handler          = "rust-api"
   source_code_hash = "${base64sha256(file("../lambdas/rust-api.zip"))}"
   runtime          = "go1.x"
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "rust-api"
+}
+
+# Root
+resource "aws_api_gateway_method" "method_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration_root" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  http_method             = "${aws_api_gateway_method.method_root.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.rust-api_lambda.arn}/invocations"
+}
+
+# /{proxy+}
+resource "aws_api_gateway_resource" "resource" {
+  path_part = "{proxy+}"
+  parent_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.rust-api_lambda.arn}/invocations"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGatewayRoot"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.rust-api_lambda.arn}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/*"
 }
