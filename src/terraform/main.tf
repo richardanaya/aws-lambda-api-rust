f3239c12675fef87b302caa4100ad74ba2916b3a
richard <richard>
2018-08-27 05:51:46 +0000

    fix

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -22,7 +22,7 @@ resource "aws_lambda_function" "rust-api_lambda" {
   filename         = "../lambdas/rust-api.zip"
   function_name    = "rust-api"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "rust-api.main"
+  handler          = "rust-api"
   source_code_hash = "${base64sha256(file("../lambdas/rust-api.zip"))}"
   runtime          = "go1.x"
 }
