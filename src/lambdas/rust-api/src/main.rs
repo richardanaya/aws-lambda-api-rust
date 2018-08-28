ce4aa60e4c8fe6af315e8589b547d063f43c2c2a
richard <richard>
2018-08-28 06:50:32 +0000

    making call to postgresql

--- src/lambdas/rust-api/src/main.rs
+++ src/lambdas/rust-api/src/main.rs
@@ -3,23 +3,38 @@ extern crate aws_lambda as lambda;
 extern crate serde_json;
 extern crate rusoto_core;
 extern crate rusoto_secretsmanager;
+extern crate postgres;
+use postgres::{Connection, TlsMode};
 
 use rusoto_core::Region;
 use rusoto_secretsmanager::{SecretsManager, SecretsManagerClient,GetSecretValueRequest};
 
+pub fn establish_connection() -> Connection {
+    let secretclient = SecretsManagerClient::new(Region::UsEast1);
+    let mut secret_request = GetSecretValueRequest::default();
+    secret_request.secret_id = "elephantsql_connection".to_owned();
+    let response = secretclient.get_secret_value(secret_request).sync().unwrap();
+    let connection_string = response.secret_string.unwrap();
+    Connection::connect(connection_string, TlsMode::None).unwrap()
+}
+
+struct Movie {
+    title: String
+}
+
 fn main() {
     // start the runtime, and return a greeting every time we are invoked
     lambda::start(|()|{
-        let secretclient = SecretsManagerClient::new(Region::UsEast1);
-        let mut secret_request = GetSecretValueRequest::default();
-        secret_request.secret_id = "elephantsql_connection".to_owned();
-        let connection_string = secretclient.get_secret_value(secret_request).sync()?.secret_string.unwrap();
-        Ok(
-        json!({
+        let conn = establish_connection();
+        for row in &conn.query("SELECT title FROM movies ", &[]).unwrap() {
+            let movie = Movie {
+                title: row.get(0)
+            };
+            println!("Found movie {}",movie.title);
+        }
+        Ok(json!({
           "statusCode":200,
-          "body":connection_string
-      })
-    )
-}
-    )
+          "body":"Connected!"
+        }))
+    })
 }
