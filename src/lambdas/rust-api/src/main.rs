8766feee823bc58966d0743a9cdebc85a3107869
richard <richard>
2018-08-29 14:40:23 +0000

    better error handling

--- src/lambdas/rust-api/src/main.rs
+++ src/lambdas/rust-api/src/main.rs
@@ -18,17 +18,17 @@ struct Movie {
     title: String
 }
 
-pub fn establish_connection() -> PgConnection {
+pub fn establish_connection() -> Result<PgConnection,ConnectionError> {
     let secretclient = SecretsManagerClient::new(Region::UsEast1);
     let mut secret_request = GetSecretValueRequest::default();
     secret_request.secret_id = "elephantsql_connection".to_owned();
     let response = secretclient.get_secret_value(secret_request).sync().unwrap();
     let connection_string = response.secret_string.unwrap();
-    PgConnection::establish(&connection_string).expect(&format!("Error connecting to postgresql"))
+    PgConnection::establish(&connection_string)
 }
 
 fn main() {
-    let connection = establish_connection();
+    let connection = establish_connection().expect("Could not connect to database.");
     lambda::start(move |()|{
         let results = diesel::sql_query("select * from movies").load::<Movie>(&connection)?;
         Ok(json!({
