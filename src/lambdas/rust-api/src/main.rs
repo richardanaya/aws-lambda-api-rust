38ab0e616d50e9379bdd61c055c020df07d77c37
Sam Rijs <srijs@airpost.net>
2018-09-03 08:59:55 +1000

    use lambda::gateway support

--- src/lambdas/rust-api/src/main.rs
+++ src/lambdas/rust-api/src/main.rs
@@ -3,7 +3,6 @@ extern crate rusoto_core;
 extern crate rusoto_secretsmanager;
 #[macro_use]
 extern crate failure;
-#[macro_use]
 extern crate serde_json;
 #[macro_use]
 extern crate diesel;
@@ -14,7 +13,7 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use diesel::sql_types::*;
 use failure::Error;
-use lambda::event::apigw::ApiGatewayProxyRequest;
+use lambda::gateway::{Request, Response};
 use lambda::Context;
 
 mod db;
@@ -26,27 +25,27 @@ struct Movie {
 }
 
 /// Handle API Gateway request and return API Gateway response.
-fn handle_request(e: ApiGatewayProxyRequest, _ctx: Context) -> Result<serde_json::Value, Error> {
+fn handle_request(req: Request, _ctx: Context) -> Result<Response, Error> {
     let connection = &db::CONNECTION.lock().unwrap() as &PgConnection;
     let movies = diesel::sql_query("select * from movies").load::<Movie>(connection)?;
 
-    Ok(json!({
-      "statusCode": 200,
-      "body": format!(
-          "List of movies for url path {}: {}",
-          e.path, // The path of the current url (e.g. /index.html )
-          movies  // Join the list of movie names with a ', '
-              .iter()
-              .map(|m| m.title.to_string())
-              .collect::<Vec<_>>()
-              .join(", "))
-          }))
+    Ok(lambda::gateway::response().status(200).body(
+        format!(
+            "List of movies for url path {}: {}",
+            req.uri().path(), // The path of the current url (e.g. /index.html )
+            movies  // Join the list of movie names with a ', '
+                .iter()
+                .map(|m| m.title.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        ).into(),
+    )?)
 }
 
 /// Start listening for AWS Lambda requests for API Gateway.
 fn main() {
-    lambda::start(move |e: ApiGatewayProxyRequest| {
+    lambda::gateway::start(|req| {
         let ctx = Context::current();
-        handle_request(e, ctx)
+        handle_request(req, ctx)
     })
 }
